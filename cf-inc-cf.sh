58a43ff1af8b4d11ac856cb8fea513d974bad124
Jordan Trask <jtrask@gmail.com>
2025-11-18 07:57:15 -0500

    chore: Removed duplicate code.

--- cf-inc-cf.sh
+++ cf-inc-cf.sh
@@ -18,18 +18,6 @@ TA=$'\t'
 CF_API_ENDPOINT=https://api.cloudflare.com/client/v4
 APIv4_ENDPOINT=$CF_API_ENDPOINT # Remove eventually
 
-# -- Colors
-BLACK="\e[30m"
-RED="\e[31m"
-GREEN="\e[32m"
-CYAN="\e[36m"
-BLUEBG="\e[44m"
-YELLOW="\e[33m"
-YELLOWBG="\e[43m"
-DARKGREYBG="\e[100m"
-DARKGRAYFG="\e[90m"
-ECOL="\e[0m"
-
 # ===============================================
 # -- Help Files
 # ===============================================
@@ -41,12 +29,12 @@ HELP_VERSION="Version: $VERSION"
 # -----------------------------------------------
 HELP_OPTIONS="Options:
 ---------
-   --details, -d       Display detailed info where possible
-   --debug, -D         Display API debugging info
-   --debug-curl, -DC   Display API debugging info and curl output
-   --quiet, -q         Less verbose
-   -E <email>          Cloudflare Email
-   -T <api_token>      Cloudflare API Token"
+    --details, -d       Display detailed info where possible
+    --debug, -D         Display API debugging info
+    --debug-curl, -DC   Display API debugging info and curl output
+    --quiet, -q         Less verbose
+    -E <email>          Cloudflare Email
+    -T <api_token>      Cloudflare API Token"
 
 # -----------------------------------------------
 # -- HELP_FULL
@@ -641,53 +629,6 @@ json_decode() {
 # TODO - Add jq_decode function that utilizes jq versus PHP
 
 # -----------------------------------------------
-# -- Messaging Functions
-# -----------------------------------------------
-_error () { echo -e "${RED}** ERROR ** - $1 ${ECOL}"; }
-_success () { echo -e "${GREEN}** SUCCESS ** - ${*} ${ECOL}"; }
-_warning () { echo -e "${YELLOW}** WARNING ** - ${*} ${ECOL}"; }
-_notice () { echo -e "${CYAN}** NOTICE ** - ${*} ${ECOL}"; }
-_running () { echo -e "${YELLOWBG}${BLACK}${*}${ECOL}"; }
-_running2 () { echo -e "${DARKGREYBG}${*}${ECOL}"; }
-_running3 () { echo -e "${DARKGRAYFG}${*}${ECOL}"; }
-_creating () {  echo -e "${DARKGREYBG}${*}${ECOL}"; }
-_separator () {  echo -e "${YELLOWBG}****************${ECOL}"; }
-
-# -----------------------------------------------
-# -- debug - ( $MESSAGE, $LEVEL)
-# -----------------------------------------------
-function _debug () {		
-	local DEBUG_MSG="$@"
-	local DEBUG_MSG_OUTPUT=""
-
-	# Get previous calling function
-	local PREV_CALLER=$(caller 1)
-	local PREV_CALLER_NAME=$(echo "$PREV_CALLER" | awk '{print $2}')
-
-	if [ "$DEBUG" = "1" ]; then
-		if [[ $DEBUG_CURL_OUTPUT = "1" ]]; then
-			DEBUG_MSG_OUTPUT+="CURL_OUTPUT: $CURL_OUTPUT_GLOBAL"
-		fi
-		for arg in "$@"; do
-			if [[ "$(declare -p "$arg" 2>/dev/null)" =~ "declare -a" ]]; then
-				DEBUG_MSG_OUTPUT+="Array contents:"
-				for item in "${arg[@]}"; do
-				DEBUG_MSG_OUTPUT+="${item}"
-				done
-			else
-				DEBUG_MSG_OUTPUT+="${arg}"
-			fi
-		done
-		echo -e "${CYAN}** DEBUG: ${PREV_CALLER_NAME}: ${DEBUG_MSG_OUTPUT}${ECOL}" >&2
-	fi
-
-	if [[ $DEBUG_FILE == "1" ]]; then
-		DEBUG_FILE_PATH="$HOME/cloudflare-cli-debug.log"
-		echo -e "${PREV_CALLER_NAME}:${DEBUG_MSG_OUTPUT}" >> "$DEBUG_FILE_PATH"
-	fi
-}
-
-# -----------------------------------------------
 # -- _die
 # -----------------------------------------------
 function _die () {
@@ -719,7 +660,7 @@ function _escape_string () {
 # ===============================================
 function _check_quiet () {
     if [[ $QUIET == "1" ]]; then
-        echo -e "${CYAN}** DEBUG: Quiet is on${ECOL}"
+        echo -e "${CYAN}** DEBUG: Quiet is on${NC}"
     fi
 }
 
